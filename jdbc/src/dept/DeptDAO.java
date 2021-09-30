9fb602af195130be3101e06e016daf7cbfe5e353
ccg <chaechg88@gmail.com>
2021-09-30 15:58:42 +0900

    데이타베이스 연결

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,43 @@
 package dept;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DeptDAO {
+	//empDAO static, Connection
+	static {
+		try {
+			Class.forName("oracle.jdbc,OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1524:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+		
+	}
+		
+		
+
+
+
+
+
+
+
+
+
 
 }
